f6e352f51c01685781e11e4917003dc800f56017
Khawer Kamran <k.kamran3@gmail.com>
2025-08-16 08:01:46 +0500

    Your commit message here

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.explicitintent.ui.theme.ExplicitIntentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMain = findViewById<Button>(R.id.button)
+
+        btnMain.setOnClickListener{
+            val myIntent = Intent(this, SecondActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+}
